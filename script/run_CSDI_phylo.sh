06729b95ffe9ed94e5d96000c9e5e29824c16536
misatoseki <misato-s@ims.u-tokyo.ac.jp>
2025-05-06 02:11:32 +0900

    First commit

---
+++ script/run_CSDI_phylo.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#$ -cwd
+
+cd ../CSDI_phylo
+LOGPATH=../logs
+
+MISSING_RATE="0.1"
+
+LOG="${LOGPATH}/CSDI_phylo_MR-${MISSING_RATE}.log"
+python3 exe.py \
+    --testmissingratio $MISSING_RATE \
+    --batch_size 8 \
+    --epochs 3000 \
+    --dataset_path "../indata/DIABIMMUNE_16S/rel-species-table_clr.csv" \
+    --mask_path "../indata/DIABIMMUNE_16S/mask_${MISSING_RATE}.csv" | tee $LOG
+
